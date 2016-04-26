d5a592336739d94019fa646e25243bdffaf1afc5
Hou Ken / Bao Qian <houkensjtu@gmail.com>
2016-04-26 13:37:43 +0900

    Seperate subroutines to extenal files. Plus new makefile.

---
+++ src/fct.f
@@ -0,0 +1,27 @@
+	SUBROUTINE FCT(X,Y,DERY)
+!     This subroutine is a "MUST" for Runge-Kutta subroutine.
+!     Look http://media.ibm1130.org/1130-006-ocr.pdf for detailed description.
+!     Here, the right hand side of the differential equation, namely y'(x) - dery(3)
+!     is calculated.
+
+	use data
+	implicit none
+	double precision y, dery, prmt, x
+	DIMENSION Y(3), DERY(3), PRMT(8)
+
+	V2=0.5*VT*(1.0-COS(OMEGA*X-PHAI))
+	V1=VT-V2
+	P1=(C1+Y(1)-Y(2)-Y(3))*R/(VRH/TRH+V1/TH)
+	P2=(C2+Y(3))*R/(VRE/TRE+V2/TE)
+	PP1=PH*PH-P1*P1
+	PP2=P1*P1-PL*PL
+!     PPX1=SIGN(1.0,PP1)*SQRT(ABS(PP1))
+!     PPX2=SIGN(1.0,PP2)*SQRT(ABS(PP2))
+	PPX1=SQRT(ABS(PP1))
+	PPX2=SQRT(ABS(PP2))
+	DERY(1)=CV(1)*PPX1
+	DERY(2)=CV(2)*PPX2
+!     DERY(3)=CV(3)*(P1*P1-P2*P2)
+	DERY(3)=CV(3)*(P1-P2)
+        RETURN
+        END SUBROUTINE
